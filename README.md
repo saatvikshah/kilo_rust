7bad5a691072722a6467567a936407d6f89b7a36
sshah1 <saatvik.shah@imc.com>
2022-03-12 13:23:36 +0100

    Finish chapter 2: Entering raw mode
    And a half assed attempt to modularize the TextEditor :/

---
+++ README.md
@@ -0,0 +1,7 @@
+## kilo-rust
+
+Listing out useful articles here:
+
+- [The Motherload](https://viewsourcecode.org/snaptoken/kilo)
+- [Check if a key is pressed](https://www.reddit.com/r/rust/comments/c8076q/check_if_a_key_is_pressed/): Suggestions on rust crates for higher or lower level control.
+- [Terminal raw mode](https://docs.rs/termion/latest/termion/raw/index.html)
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
-use std::io::{self, Read, Write};
-use termion::raw::IntoRawMode;
+use std::io::{self, Write};
+use termion::{raw::IntoRawMode, input::TermRead, event::Key};
 
 fn main() {
+    // Main program
     let stdout = io::stdout();
     let mut stdout = stdout.lock().into_raw_mode().unwrap();
     let stdin = io::stdin();
     let stdin = stdin.lock();
 
-    let mut bytes = stdin.bytes();
+    let mut keys = stdin.keys();
 
     loop{
-        let user_char = bytes.next().unwrap().unwrap();
-        match user_char {
-            b'q' => return,
-            c => write!(stdout, "{}", c as char)
+        let in_key = keys.next().unwrap().unwrap();
+        match in_key {
+            Key::Char('q') => return,
+            Key::Char(c) => {
+                if c.is_control() {
+                    // 'Enter'/'Ctrl-M': Fix carriage return ('\r\n' => '\n')
+                    writeln!(stdout)
+                } else {
+                    write!(stdout, "{}", c)
+                }
+            },
+            other => write!(stdout, "{:?}", other)
         }.unwrap();
         stdout.flush().unwrap();
     }
