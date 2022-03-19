9e9cb6d3ab57c5e5982fed2a62d8430e6993db52
sshah1 <saatvik.shah@imc.com>
2022-03-19 17:11:42 +0100

    Complete till step 24
    - Factor everything out into cleaner functions
    - Refresh screen and move cursor to start position

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,36 @@
-use std::io::{self, Write};
-use termion::{raw::IntoRawMode, input::TermRead, event::Key};
+use std::{io::{self, Write, StdinLock, StdoutLock}};
+use termion::{raw::{IntoRawMode, RawTerminal}, input::TermRead, event::Key};
+
+#[derive(PartialEq)]
+enum EventLoopState{
+    Running,
+    Terminate
+}
+
+fn editor_process_keypress(stdin: &mut StdinLock) -> EventLoopState {
+    let in_key = stdin.keys().next().unwrap().unwrap();
+    match in_key {
+        Key::Ctrl('w') => EventLoopState::Terminate,
+        _ => EventLoopState::Running
+    }
+}
+
+fn editor_refresh_screen(stdout: &mut RawTerminal<StdoutLock>) {
+    write!(stdout, "{}", termion::clear::All).unwrap();
+    write!(stdout, "{}", termion::cursor::Goto(1,1)).unwrap();
+    stdout.flush().unwrap();
+}
 
 fn main() {
     // Main program
     let stdout = io::stdout();
     let mut stdout = stdout.lock().into_raw_mode().unwrap();
     let stdin = io::stdin();
-    let stdin = stdin.lock();
-
-    let mut keys = stdin.keys();
-
-    loop{
-        let in_key = keys.next().unwrap().unwrap();
-        match in_key {
-            Key::Char('q') => return,
-            Key::Char(c) => {
-                if c.is_control() {
-                    // 'Enter'/'Ctrl-M': Fix carriage return ('\r\n' => '\n')
-                    writeln!(stdout)
-                } else {
-                    write!(stdout, "{}", c)
-                }
-            },
-            other => write!(stdout, "{:?}", other)
-        }.unwrap();
-        stdout.flush().unwrap();
+    let mut stdin = stdin.lock();
+    let mut event_loop_state = EventLoopState::Running;
+    while event_loop_state == EventLoopState::Running {
+        editor_refresh_screen(&mut stdout);
+        event_loop_state = editor_process_keypress(&mut stdin);
     }
+    editor_refresh_screen(&mut stdout);
 }
