7eb49c05a4bd1a9248c0b6263a608ca33d33d858
sshah1 <saatvik.shah@imc.com>
2022-04-01 23:12:49 +0200

    Ch4: Support vertical scrolling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use std::{io::{self, Write, StdinLock, StdoutLock}};
+use std::{io::{self, Write, StdinLock, StdoutLock}, env};
 use termion::{raw::{IntoRawMode, RawTerminal}, input::TermRead, event::Key};
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+};
 
 #[derive(PartialEq)]
 enum EventLoopState{
     Running,
     Terminate
+}
+
+struct EditorState{
+    cursor: CursorState,
+    contents: Vec<String>,
+    row_offset: usize
 }
 
 struct CursorState{
@@ -77,17 +88,28 @@ impl CursorState{
     pub fn at_bottom(self: &CursorState) -> bool {
         self.y == self.y_max
     }
+
+    pub fn at_top(self: &CursorState) -> bool {
+        self.y == 0
+    }
 }
 
-fn editor_process_keypress(stdin: &mut StdinLock, cursor: &mut CursorState) -> EventLoopState {
+fn editor_process_keypress(stdin: &mut StdinLock, editor: &mut EditorState) -> EventLoopState {
+    let cursor = &mut editor.cursor;
     let in_key = stdin.keys().next().unwrap().unwrap();
     match in_key {
         Key::Ctrl('w') => EventLoopState::Terminate,
         Key::Up => {
+            if cursor.at_top() && editor.row_offset > 0 {
+                editor.row_offset -= 1;
+            }
             cursor.sub_y(1);
             EventLoopState::Running
         },
         Key::Down => {
+            if cursor.at_bottom() && (editor.row_offset + (cursor.display_ymax() as usize)) < editor.contents.len() {
+                editor.row_offset += 1;
+            }
             cursor.add_y(1);
             EventLoopState::Running
         },
@@ -119,20 +141,30 @@ fn editor_process_keypress(stdin: &mut StdinLock, cursor: &mut CursorState) -> E
     }
 }
 
-fn editor_draw_rows(stdout: &mut RawTerminal<StdoutLock>, cursor: &CursorState) {
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
+}
+
+fn editor_draw_rows(stdout: &mut RawTerminal<StdoutLock>, editor: &mut EditorState) {
     write!(stdout, "{}", termion::cursor::Goto(1, 1)).unwrap();
-    for _ in 0..(cursor.display_ymax() - 1) {
+    
+    let cursor = &editor.cursor;
+    let drawable_rows = cursor.display_ymax() as usize;
+    let rows_from_file = editor.contents.len().min(drawable_rows);
+    for row_file in 0..rows_from_file {
+        write!(stdout, "{}{}\r\n", termion::clear::CurrentLine, editor.contents[row_file + editor.row_offset]).unwrap();
+    }
+    for _ in rows_from_file..drawable_rows {
         write!(stdout, "{}~\r\n", termion::clear::CurrentLine).unwrap();
     }
     write!(stdout, "{}~", termion::clear::CurrentLine).unwrap();
-
-    if cursor.at_bottom() {
-       return; 
-    }
-
     
     // TODO: StringBuilder like construct to create footer string 
-    let footer_message = format!("kilo-rust -- [{}/{}, {}/{}]", cursor.display_x(), cursor.display_xmax(), cursor.display_y(), cursor.display_ymax());
+    let footer_message = format!("kilo-rust -- [{}/{}, {}/{}, {}/{}]", cursor.display_x(), cursor.display_xmax(), cursor.display_y(), cursor.display_ymax(), cursor.display_y() + (editor.row_offset as u16), editor.contents.len());
     let footer_start_x = (usize::from(cursor.display_xmax()) - footer_message.len()) / 2;
     for _ in 1..footer_start_x {
         write!(stdout, " ").unwrap();
@@ -140,26 +172,35 @@ fn editor_draw_rows(stdout: &mut RawTerminal<StdoutLock>, cursor: &CursorState) 
     write!(stdout, "{}", footer_message).unwrap();
 }
 
-fn editor_refresh_screen(stdout: &mut RawTerminal<StdoutLock>, cursor: &mut CursorState) {
+fn editor_refresh_screen(stdout: &mut RawTerminal<StdoutLock>, editor: &mut EditorState) {
     write!(stdout, "{}", termion::cursor::Hide).unwrap();
-    editor_draw_rows(stdout, cursor);
-    write!(stdout, "{}", termion::cursor::Goto(cursor.display_x(), cursor.display_y())).unwrap();
+    editor_draw_rows(stdout, editor);
+    let cursor = &editor.cursor;
+    write!(stdout, "{}", termion::cursor::Goto(cursor.display_x(), editor.cursor.display_y())).unwrap();
     write!(stdout, "{}", termion::cursor::Show).unwrap();
     stdout.flush().unwrap();
 }
 
 fn main() {
-    // Main program
+    let mut args = env::args();
+    args.next();
+    let filename: String = args.next().expect("Expected filename arg");
+
+    let (xlen, ylen) = termion::terminal_size().unwrap();
+    let mut editor_state = EditorState{
+        cursor: CursorState::new(xlen, ylen - 1),
+        contents: lines_from_file(filename),
+        row_offset: 0};
+
     let stdout = io::stdout();
     let mut stdout = stdout.lock().into_raw_mode().unwrap();
     let stdin = io::stdin();
     let mut stdin = stdin.lock();
     let mut event_loop_state = EventLoopState::Running;
-    let (xlen, ylen) = termion::terminal_size().unwrap();
-    let mut cursor = CursorState::new(xlen, ylen);
+
     while event_loop_state == EventLoopState::Running {
-        editor_refresh_screen(&mut stdout, &mut cursor);
-        event_loop_state = editor_process_keypress(&mut stdin, &mut cursor);
+        editor_refresh_screen(&mut stdout, &mut editor_state);
+        event_loop_state = editor_process_keypress(&mut stdin, &mut editor_state);
     }
-    editor_refresh_screen(&mut stdout, &mut cursor);
+    editor_refresh_screen(&mut stdout, &mut editor_state);
 }
